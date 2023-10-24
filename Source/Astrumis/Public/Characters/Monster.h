c24f42a0736655d76843cdc9bbf4b461b2416b2a
tyci <tycjan.woronko@polyslash.com>
2023-10-24 15:59:18 +0200

    refactor: removed unnecessary lines

--- Source/Astrumis/Public/Characters/Monster.h
+++ Source/Astrumis/Public/Characters/Monster.h
@@ -10,21 +10,4 @@ UCLASS()
 class ASTRUMIS_API AMonster : public ACharacter
 {
 	GENERATED_BODY()
-
-public:
-	// Sets default values for this character's properties
-	AMonster();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
-	virtual void PossessedBy(AController* NewController) override;
 };
